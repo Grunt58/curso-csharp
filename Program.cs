fb7e9d81a66a44b76561df8459c027e85f466c48
G58 <freddy.fazbear.troll@gmail.com>
2024-12-31 13:08:36 -0600

    code: Aplicando genéricos de tipo Alumno a tipo string

--- Program.cs
+++ Program.cs
@@ -7,29 +7,31 @@ namespace Clase
         static void Main(string[] args)
         {
             // Variables locales
-            Alumno valorElemento;
+            string valorElemento;
 
-            // Instanciando la clase
-            GuardaObjetos<Alumno> objetos1 = new GuardaObjetos<Alumno>(3);
+            //// Instanciando la clase
+            //GuardaObjetos<Alumno> objetos1 = new GuardaObjetos<Alumno>(3);
 
             // Instanciamos a la clase "Alumno"
             Alumno alumno1 = new Alumno(8.5);
             Alumno alumno2 = new Alumno(10);
             Alumno alumno3 = new Alumno(6.8);
 
-            // Agregar objetos a la clase "GuardarObjetos"
-            objetos1.AgregarElementos(alumno1);
-            objetos1.AgregarElementos(alumno2);
-            objetos1.AgregarElementos(alumno3);
+            //// Agregar objetos a la clase "GuardarObjetos"
+            //objetos1.AgregarElementos(alumno1);
+            //objetos1.AgregarElementos(alumno2);
+            //objetos1.AgregarElementos(alumno3);
 
-            //// Agregar objetos
-            //objetos1.AgregarElementos("Luis");
-            //objetos1.AgregarElementos("José");
-            //objetos1.AgregarElementos("Juan");
+            GuardaObjetos<string> objetos1 = new GuardaObjetos<string>(3);
+
+            // Agregar objetos
+            objetos1.AgregarElementos("Luis");
+            objetos1.AgregarElementos("José");
+            objetos1.AgregarElementos("Juan");
 
             // Obtener elemento
             valorElemento = objetos1.ObtenerElemento(2);
-            Console.WriteLine(valorElemento.Calificacion);
+            Console.WriteLine(valorElemento);
         }
     }
 
