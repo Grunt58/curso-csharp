9b06e70ab8d4621b3ad9be082c329f1ac0bbe348
G58 <freddy.fazbear.troll@gmail.com>
2024-12-31 19:59:00 -0600

    code: Agregar elementos a la List

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace Clase
     {
         static void Main(string[] args)
         {
-            List<T> NombreLista = new List<T>();
+            // Variables necesarias
+            string elem;
+            int opcion;
+
+            // Instancia de List
+            List<string> Personas = new List<string>();
+
+            // Agregamos objetos a la lista
+            Personas.Add("Hugo");
+            Personas.Add("Miguel");
+            Personas.Add("Diana");
+            Personas.Add("Erick");
+
+            do
+            {
+                Console.WriteLine("1. Agregar elemento");
+                Console.WriteLine("2. Salir");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                if (opcion == 1)
+                {
+                    Console.Write("Ingresa el elemento a agregar en la lista: ");
+                    elem = Console.ReadLine();
+
+                    Personas.Add(elem);
+                }
+            } while (opcion != 2);
         }
     }
 }
