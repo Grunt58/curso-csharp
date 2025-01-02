ba686b0e1d60cb1d41d83173bccbe932a92beffb
G58 <freddy.fazbear.troll@gmail.com>
2025-01-02 00:52:11 -0600

    code: Preparación para ejercicio con List

--- Program.cs
+++ Program.cs
@@ -7,47 +7,7 @@ namespace Clase
     {
         static void Main(string[] args)
         {
-            // Variables necesarias
-            string elem;
-            int opcion;
-
-            // Instancia de List
-            List<string> Personas = new List<string>();
-
-            // Agregamos objetos a la lista
-            Personas.Add("Hugo");
-            Personas.Add("Miguel");
-            Personas.Add("Diana");
-            Personas.Add("Héctor");
-
-            //do
-            //{
-            //    Console.WriteLine("1. Agregar elemento");
-            //    Console.WriteLine("2. Salir");
-
-            //    opcion = Convert.ToInt32(Console.ReadLine());
-
-            //    if (opcion == 1)
-            //    {
-            //        Console.Write("Ingresa el elemento a agregar en la lista: ");
-            //        elem = Console.ReadLine();
-
-            //        Personas.Add(elem);
-            //    }
-
-            //} while (opcion != 2);
-
-            Console.WriteLine("Información en la List: \n");
-            // Instrucción "foreach" para recorrer una colección
-            foreach (string elemento in Personas)
-            {
-                Console.WriteLine(elemento);
-            }
-
-            Console.WriteLine("Ejecutando el método 'Clear()'");
-            Personas.Clear();
-
-            Console.WriteLine($"\nElementos en la List: {Personas.Count}");
+            
         }
     }
 }
