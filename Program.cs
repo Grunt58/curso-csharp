807a86e4557b5b3a9c7b6e6c9a5db691dcf4fb84
G58 <freddy.fazbear.troll@gmail.com>
2025-01-11 02:04:34 -0600

    Colección genérica 'Dictionary<TKey,TValue>'

--- Program.cs
+++ Program.cs
@@ -8,46 +8,8 @@ namespace Clase
     {
         static void Main(string[] args)
         {
-            // Queue o cola
-            // El primero en entrar, el primero en salir
-            Queue<char> miFila = new Queue<char>();
-
-            // Agregando objetos al Queue
-            miFila.Enqueue('a');
-            miFila.Enqueue('b');
-            miFila.Enqueue('c');
-            miFila.Enqueue('d');
-
-            int i = 0;
-
-            // Recorriendo la Queue
-            foreach (char elemento in miFila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-
-            // Agregando otro elemento
-            Console.WriteLine("\nDespués de agregar a (e)\n");
-            miFila.Enqueue('e');
-
-            i = 0;
-            // Recorriendo la Queue
-            foreach (char elemento in miFila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-
-            // Después de quitar un elemento
-            Console.WriteLine("Después de quitar el elemento\n");
-            var objElim = miFila.Dequeue();
-
-            i = 0;
-            // Recorriendo la Queue
-            foreach (char elemento in miFila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-            Console.WriteLine($"\n({objElim}) fue eliminado correctamente");
+            // Dictionary<TKey,TValue> (Diccionario)
+            Dictionary<string, int> empleados = new Dictionary<string, int>();
         }
     }
 }
