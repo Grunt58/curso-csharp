2f5b6643b04c5842d0095dd51a64a4135a7f0d30
G58 <freddy.fazbear.troll@gmail.com>
2025-01-11 00:46:31 -0600

    Colección genérica 'Queue<T>'

--- Program.cs
+++ Program.cs
@@ -8,85 +8,10 @@ namespace Clase
     {
         static void Main(string[] args)
         {
-            // Stack
-            Stack<double> miPila = new Stack<double>();
+            // Queue o cola
+            // El primero en entrar, el primero en salir
+            Queue<T> nombre = new Queue<T>;
 
-            // Insertar objetos al principio del Stack
-            miPila.Push(5.9);
-            miPila.Push(13.1);
-            miPila.Push(8.7);
-            miPila.Push(3.2);
-            miPila.Push(10.5);
-
-            int i = 0; // Mostrar índice
-
-            // Mostrar la información del Stack
-            foreach(double elemento in miPila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-
-            // Después de insertar un elemento
-            Console.WriteLine("\nDespués de insertar un elemento\n");
-            miPila.Push(1.5);
-            i = 0;
-
-            // Mostrar la información del Stack
-            foreach(double elemento in miPila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-
-            // Después de quitar un elemento
-            Console.WriteLine("\nDespués de quitar un elemento\n");
-            var eliminado = miPila.Pop();
-            i = 0; // Reiniciamos el valor del índice
-
-            // Mostrar la información del Stack
-            foreach(double elemento in miPila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-
-            Console.WriteLine($"El elemento ({eliminado}) se ha eliminado con éxito.");
-
-            //Después de usar Peek()
-            Console.WriteLine("\nDespués de usar Peek()\n");
-            var primerObj = miPila.Peek();
-            i = 0; // Reiniciamos el valor del índice
-
-            // Mostrar la información del Stack
-            foreach (double elemento in miPila)
-            {
-                Console.WriteLine($"[{i++}]: {elemento}");
-            }
-
-            Console.WriteLine($"El primer elemento del Stack es: ({primerObj})");
-
-            // Saber si el Stack contiene el elemento
-            bool contiene;
-            double buscarElem;
-
-            Console.Write("\nIngresa el elemento a buscar: ");
-            buscarElem = Convert.ToDouble(Console.ReadLine());
-
-            contiene = miPila.Contains(buscarElem);
-
-            if (contiene)
-            {
-                Console.WriteLine($"El objeto ({buscarElem}) se encuentra en el Stack");
-            }
-            else
-            {
-                Console.WriteLine($"No se encontró el objeto ({buscarElem}) en el Stack");
-            }
-
-            Console.WriteLine($"\nEl Stack contiene {miPila.Count} elementos.");
-
-            // Borrando el Stack
-            Console.WriteLine("\nDespués de borrar el Stack");
-            miPila.Clear();
-            Console.WriteLine($"\nEl Stack contiene {miPila.Count} elementos.");
         }
     }
 }
