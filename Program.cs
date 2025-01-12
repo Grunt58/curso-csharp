c08cde18e38209089d03c5947909655a2215a967
G58 <freddy.fazbear.troll@gmail.com>
2025-01-12 01:08:42 -0600

    Propiedad Item[] y su excepeción

--- Program.cs
+++ Program.cs
@@ -20,6 +20,32 @@ namespace Clase
                 Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
             }
 
+            // colección[key] = value;
+            empleados["Miguel"] = 45; // Con Add: empleados.Add("Miguel", 45);
+
+            // Después de agregar un elemento con Item[]
+            Console.WriteLine("\nDespués de agregar un elemento con Item[]\n");
+
+            foreach (KeyValuePair<string, int> elemento in empleados)
+            {
+                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
+            }
+
+            // Comprobando que Item[] reemplaza la "key"
+            Console.WriteLine("\nComprobando que Item[] reemplaza la (key)\n");
+
+            empleados["Hugo"] = 19;
+
+            foreach (KeyValuePair<string, int> elemento in empleados)
+            {
+                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
+            }
+
+            // Usando el get de la propiedad Item[]
+            Console.WriteLine($"\nLa edad de Erica es: {empleados["Erica"]}\n");
+
+            // Si no se encuetra la clave
+            Console.WriteLine($"\nLa edad de Erica es: {empleados["Maria"]}\n");
         }
     }
 }
