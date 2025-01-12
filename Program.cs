8b1469c955e872ae075c2519fdfe953074d85a20
G58 <freddy.fazbear.troll@gmail.com>
2025-01-12 00:46:28 -0600

    Método Add() y estructura de diccionario

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace Clase
         {
             // Dictionary<TKey,TValue> (Diccionario)
             Dictionary<string, int> empleados = new Dictionary<string, int>();
+
+            // Añadiando key-value pairs a la colección
+            empleados.Add("Hugo", 36);
+            empleados.Add("Erica", 20);
+
+            foreach (KeyValuePair<string, int> elemento in empleados)
+            {
+                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
+            }
+
         }
     }
 }
