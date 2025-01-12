c7b15f857ef42bff207150bdb1561766a50bb66e
G58 <freddy.fazbear.troll@gmail.com>
2025-01-12 01:39:50 -0600

    Ejercicio

--- Program.cs
+++ Program.cs
@@ -8,75 +8,7 @@ namespace Clase
     {
         static void Main(string[] args)
         {
-            // Dictionary<TKey,TValue> (Diccionario)
-            Dictionary<string, int> empleados = new Dictionary<string, int>();
-
-            // Añadiando key-value pairs a la colección
-            empleados.Add("Hugo", 36);
-            empleados.Add("Erica", 20);
-
-            foreach (KeyValuePair<string, int> elemento in empleados)
-            {
-                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
-            }
-
-            // colección[key] = value;
-            empleados["Miguel"] = 45; // Con Add: empleados.Add("Miguel", 45);
-
-            // Después de agregar un elemento con Item[]
-            Console.WriteLine("\nDespués de agregar un elemento con Item[]\n");
-
-            foreach (KeyValuePair<string, int> elemento in empleados)
-            {
-                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
-            }
-
-            // Comprobando que Item[] reemplaza la "key"
-            Console.WriteLine("\nComprobando que Item[] reemplaza la (key)\n");
-
-            empleados["Hugo"] = 19;
-
-            foreach (KeyValuePair<string, int> elemento in empleados)
-            {
-                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
-            }
-
-            // Usando el get de la propiedad Item[]
-            Console.WriteLine($"\nLa edad de Erica es: {empleados["Erica"]}\n");
-
-            // Si no se encuetra la clave
-            // Console.WriteLine($"\nLa edad de Erica es: {empleados["Maria"]}\n");
-
-            Console.WriteLine("\nDespués de remover un elemento\n");
-
-            empleados.Remove("Erica");
-
-            foreach (KeyValuePair<string, int> elemento in empleados)
-            {
-                Console.WriteLine($"Key = {elemento.Key}, Value = {elemento.Value}");
-            }
-
-            Console.WriteLine();
-
-            if (empleados.ContainsKey("Erica"))
-            {
-                Console.WriteLine("La key se encuentra en la colección");
-            }
-            else
-            {
-                Console.WriteLine("La key no se encuentra en la colección");
-            }
-
-            Console.WriteLine();
-
-            if (empleados.ContainsValue(50))
-            {
-                Console.WriteLine("Existe al menos alguien con esa edad");
-            }
-            else
-            {
-                Console.WriteLine("No se encotró nadie con esa edad");
-            }
+            
         }
     }
 }
